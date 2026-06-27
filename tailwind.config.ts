3c5d88e6c8c58a0280a5cead924be37ee95e3e5e
SimuImmo <ft.bu+feedback@ik.me>
2026-06-27 21:18:36 +0200

    Fix: Écrasement forcé de tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,15 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // <-- Ajoutez cette ligne !
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
